7c67f1975fc56bdf2151570a729a009719cc12e9
przemekszcz7 <przemekszcz7@gmail.com>
2026-05-19 14:07:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/nowydwor/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
